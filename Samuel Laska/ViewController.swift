76b080b852adfd72a2a9f986bc8ce1daa10904a8
Samuel Laska <samuel.laska@gmail.com>
2015-04-23 23:41:10 +0200

    Initial Commit

---
+++ Samuel Laska/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Samuel Laska
+//
+//  Created by Samuel Laska on 4/23/15.
+//  Copyright (c) 2015 Samuel Laska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
